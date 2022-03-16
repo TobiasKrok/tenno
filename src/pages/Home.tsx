3542a6b58653a1c98978a39bb28be4249f5fe901
Tobias <tobiaskk@uia.no>
2022-03-16 11:02:57 +0100

    Initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+const Home: React.FC = () => {
+    return (
+        <div className="home">
+            <div className="directory-menu">
+                <div className="menu-item"></div>
+                <h1 className="title">HATS</h1>
+                <span className="subtitle">SHOW NOW</span>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
