fd1236aff6ce3610f4ffc2ff4ffd81be35bd0f70
Tobias <tobiaskk@uia.no>
2022-03-18 11:26:20 +0100

    Add navigation styling, add SVG logo

--- src/components/molecules/Navigation.tsx
+++ src/components/molecules/Navigation.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import '../../styles/components/navigation/navigation.scss';
 const Navigation: React.FC = () => {
     return (
         <>
             <div className="navigation">
                 <Link className="logo-container" to="/">
-                    <div>Logo</div>
+                    <Logo className="logo" />
                 </Link>
                 <div className="nav-links-container">
                     <Link className="nav-link" to="/shop">
                         SHOP
                     </Link>
                 </div>
-                <Outlet />
             </div>
+            <Outlet />
         </>
     );
 };
