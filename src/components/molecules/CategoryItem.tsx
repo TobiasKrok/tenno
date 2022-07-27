e5da55580b5100784b14a04573ad0c73dfeaf58a
TobiasKrok <ekoncraft@hotmail.no>
2022-07-27 23:38:54 +0200

    Link to shop

--- src/components/molecules/CategoryItem.tsx
+++ src/components/molecules/CategoryItem.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../../styles/components/category-item/category-item.scss';
 import { Category } from '../../types/categories';
 
 const CategoryItem: React.FC<Omit<Category, 'id'>> = ({ title, imageUrl }) => {
+    const navigate = useNavigate();
+
     return (
-        <div className="category-item-container">
+        <div className="category-item-container" onClick={() => navigate('shop/' + title)}>
             <div
                 className="background-image"
                 style={{
